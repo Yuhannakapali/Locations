867b2f0806370569444914cc51cc237e389a4b11
yuhanna kapali <yuhanna.kapali@kotuko.it>
2021-07-16 00:35:09 +0545

    adding the database and config and creatuing service for the database

--- config/default.ts
+++ config/default.ts
@@ -1,10 +1,17 @@
 import { join } from "path";
 
+const SOURCE_PATH = join(__dirname,"..", "src")
+
 export default {
     template: {
         path : join(__dirname, "..", "views")
     },
-
+    database:{
+        url :"postgres://app:app@localhost/location"
+    },
+    orm:{
+        entities:[`${SOURCE_PATH}/**/*.entity.ts`],
+    },
     public :{
         path : join(__dirname, "..", "public")
     }
