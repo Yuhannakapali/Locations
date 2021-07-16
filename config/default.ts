e436eef1f977b54c59c1c5883fed3b3796be83e4
yuhanna kapali <yuhanna.kapali@kotuko.it>
2021-07-16 13:36:10 +0545

    linting the code

--- config/default.ts
+++ config/default.ts
@@ -1,19 +1,19 @@
-import { join } from "path";
+import { join } from 'path';
 
-const SOURCE_PATH = join(__dirname,"..", "src")
+const SOURCE_PATH = join(__dirname, '..', 'src');
 
 export default {
-    template: {
-        path : join(__dirname, "..", "views")
-    },
-    database:{
-        url :"postgres://app:secret@localhost/locations",
-        synchronize: false,
-    },
-    orm:{
-        entities:[`${SOURCE_PATH}/**/*.entity.ts`],
-    },
-    public :{
-        path : join(__dirname, "..", "public")
-    }
-}
+  template: {
+    path: join(__dirname, '..', 'views'),
+  },
+  database: {
+    url: 'postgres://app:secret@localhost/locations',
+    synchronize: false,
+  },
+  orm: {
+    entities: [`${SOURCE_PATH}/**/*.entity.ts`],
+  },
+  public: {
+    path: join(__dirname, '..', 'public'),
+  },
+};
--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -4,11 +4,11 @@ import { ConfigModule } from 'src/config/config.module';
 import { PostgresTypeOrmConfigService } from './postgres-typeorm-config.service';
 
 @Module({
-    imports: [
-        TypeOrmModule.forRootAsync({
-            imports:[ConfigModule],
-            useClass: PostgresTypeOrmConfigService
-        })
-    ]
+  imports: [
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useClass: PostgresTypeOrmConfigService,
+    }),
+  ],
 })
 export class DatabaseModule {}
